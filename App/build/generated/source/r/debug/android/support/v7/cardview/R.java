90db8e16aba93165767571cd75a137ce5f1c65c5
Casey <cjbrooks12@gmail.com>
2015-03-04 00:44:51 -0600

    modified build.gradle files to get ABT as a remote dependency

--- App/build/generated/source/r/debug/android/support/v7/cardview/R.java
+++ App/build/generated/source/r/debug/android/support/v7/cardview/R.java
@@ -32,9 +32,9 @@ public final class R {
 		public static final int cardview_default_radius = 0x7f0b0030;
 	}
 	public static final class style {
-		public static final int CardView = 0x7f0f0061;
-		public static final int CardView_Dark = 0x7f0f0062;
-		public static final int CardView_Light = 0x7f0f0063;
+		public static final int CardView = 0x7f0f0062;
+		public static final int CardView_Dark = 0x7f0f0063;
+		public static final int CardView_Light = 0x7f0f0064;
 	}
 	public static final class styleable {
 		public static final int[] CardView = { 0x7f01003f, 0x7f010040, 0x7f010041, 0x7f010042, 0x7f010043, 0x7f010044, 0x7f010045, 0x7f010046, 0x7f010047, 0x7f010048, 0x7f010049 };
